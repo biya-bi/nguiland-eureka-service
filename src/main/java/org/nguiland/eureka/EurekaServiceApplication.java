56569f761cbfd0dd75009e9764de9c5ec2456ef7
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-09-09 19:22:21 -0400

    Rename base package to org.nguiland

--- src/main/java/org/nguiland/eureka/EurekaServiceApplication.java
+++ src/main/java/org/nguiland/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.optimagrowth.eureka;
+package org.nguiland.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
